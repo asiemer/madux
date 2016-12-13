b5eac6c0f1e37e8e48567fd8a9ea29cb1f9d64c0
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-13 15:09:31 +0100

    Update state and tests

--- src/State.js
+++ src/State.js
@@ -1,13 +1,21 @@
 
 // @flow
 
+import { Props } from './Props';
+import type { Prop, PropDefinition } from './Types';
+
 class State {
 
   name: string;
+  props: Props;
 
-  constructor(name: string) {
+  constructor(name: string, props: Array<PropDefinition>): void {
     this.name = name;
+    this.props = new Props(props);
   }
+
+  addProp(prop: PropDefinition): void { this.props.addProp(prop); }
+  validate(props: Prop): boolean { return this.props.validate(props); }
 
 }
 
