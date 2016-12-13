ff03614fe9f9e9d91834c0967a734e932afe9b29
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-14 00:54:16 +0100

    Finish tests

--- build/Madux.js
+++ build/Madux.js
@@ -3,15 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.createStoreWithMiddleWare = exports.createStore = undefined;
 
 var _Store = require('./Store');
 
 var _Machine = require('./Machine');
 
-var createStore = exports.createStore = function createStore(machine) {
+// Creates a new store.
+exports.default = function (machine) {
   return new _Store.Store(machine);
-};
-var createStoreWithMiddleWare = exports.createStoreWithMiddleWare = function createStoreWithMiddleWare(ma, mi) {
-  return new _Store.Store(ma, mi);
 };
--- src/Madux.js
+++ src/Madux.js
@@ -3,7 +3,6 @@
 
 import { Store } from './Store';
 import { Machine } from './Machine';
-import type { Middleware } from './Types';
 
-export const createStore = (machine: Machine) => new Store(machine);
-export const createStoreWithMiddleWare = (ma: Machine, mi: Array<Middleware>) => new Store(ma, mi);
+// Creates a new store.
+export default (machine: Machine) => new Store(machine);
