c21cdc802e6c65b9d780acff71002e63687899cc
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-13 15:14:58 +0100

    Add basic Machine

---
+++ src/Machine.js
@@ -0,0 +1,24 @@
+
+// @flow
+
+import { State } from './State';
+
+class Machine {
+
+  current: ?string;
+  initial: ?string;
+  states: Map<string, State>;
+  structure: Map<string, Map<string, string>>;
+
+  constructor(states: Array<State>) {
+    if (states.length < 1) { throw new Error('You need at least one state!'); }
+    states.forEach((state) => {
+      if (!this.initial) this.initial = state.name;
+      this.states.set(state.name, state);
+      this.structure.set(state.name, new Map());
+    });
+  }
+
+}
+
+exports.Machine = Machine;
