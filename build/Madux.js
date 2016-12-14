8d3f09c28762d50e84f5b5c729dc5a47e69c01ff
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-14 23:06:12 +0100

    Update build

--- build/Madux.js
+++ build/Madux.js
@@ -10,6 +10,11 @@ exports.createStore = function (machine) {
   return new _Store.Store(machine);
 };
 
+exports.createMachine = function (states) {
+  var middlewares = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  return new _Machine.Machine(states, middlewares);
+};
+
 exports.Machine = _Machine.Machine;
 exports.State = _State.State;
 exports.Store = _Store.Store;
