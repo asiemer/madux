38e36ef76124257cedb7caa5ec6805557f242f27
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-17 09:54:12 +0100

    Update Types and add Utils for typechecking

--- src/Types.js
+++ src/Types.js
@@ -1,29 +1,20 @@
 
 // @flow
 
-// Defines a Prop of a State. It only requires a name and
-// a boolean on whether or not this Prop is required.
-export type PropDefinition = {
+export type StatePropDefinition = {
   name: string;
   required: boolean;
 };
 
-// A Prop is an instance of a PropDefinition. It is just
-// a regular JS object.
-export type Prop = Object;
+export type State = {
+  name: string;
+  props?: Array<StatePropDefinition>;
+};
 
-// An Action is the type that will be passed to the
-// dispatch method of the Store. It has a type and
-// optional parameters.
 export type Action = {
   type: string;
   params: Object;
 };
 
-// The format of the dispatch function. Used to define
-// it within the middlewares.
 export type Dispatch = (action: Action) => void;
-
-// A middleware function takes a dispatch as input and
-// returns a new wrapped dispatch method.
 export type Middleware = (disp: Dispatch) => Dispatch;
---
+++ src/Utils.js
@@ -0,0 +1,42 @@
+
+// @flow
+
+const OBJSTR = '[object Object]';
+const ARRSTR = '[object Array]';
+
+export const isDict = (data: any = []): boolean =>
+  data && Object.prototype.toString.call(data) === OBJSTR;
+
+export const isArr = (data: any = []): boolean =>
+  data && Object.prototype.toString.call(data) === ARRSTR;
+
+export const isValidPropDefinition = (prop: any = {}): boolean =>
+  isDict(prop)
+  && ('name' in prop)
+  && typeof prop.name === 'string'
+  && ('required' in prop ? typeof prop.required === 'boolean' : true);
+
+export const areValidPropDefinitions = (props: Array<any> = []): boolean =>
+  props && isArr(props) && props.reduce((bool, prop) => bool && isValidPropDefinition(prop), true);
+
+export const isValidState = (state: any = {}): boolean =>
+  isDict(state)
+  && ('name' in state)
+  && typeof state.name === 'string'
+  && ('props' in state ? areValidPropDefinitions(state.props) : true);
+
+export const isValidAction = (action: any = {}): boolean =>
+  isDict(action)
+  && ('name' in action)
+  && typeof action.type === 'string'
+  && ('params' in action)
+  && isDict(action.params);
+
+export const areValidPropsForPropDefinitions = (params: any = {}, definitions: Array<any> = []) =>
+  areValidPropDefinitions(definitions)
+  && definitions.reduce((bool, def) => bool && (def.required ? def.name in params : true), true);
+
+export const isValidActionForState = (action: any = {}, state: any = {}): boolean =>
+  isValidAction(action)
+  && isValidState(state)
+  && ('props' in state ? areValidPropsForPropDefinitions(action.params, state.props) : true);
