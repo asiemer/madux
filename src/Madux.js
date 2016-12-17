eff3102f2eeb158feeb54c50ded35269faf943d5
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-17 14:59:01 +0100

    Update Madux

--- src/Madux.js
+++ src/Madux.js
@@ -1,17 +1,6 @@
 
 // @flow
 
-import { Store } from './Store';
-import { State } from './State';
-import { Machine } from './Machine';
-import type { Middleware, PropDefinition } from './Types';
+import { createMachine } from './Machine';
 
-exports.createStore = (machine: Machine, middlewares: Array<Middleware> = []) =>
-  new Store(machine, middlewares);
-
-exports.createMachine = (states: Array<State>) => new Machine(states);
-exports.createState = (name: string, props: Array<PropDefinition> = []) => new State(name, props);
-
-exports.Machine = Machine;
-exports.State = State;
-exports.Store = Store;
+exports.createMachine = createMachine;
