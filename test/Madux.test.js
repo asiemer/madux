58cd15f013f2b51098190ccf77b3da4acf000187
Jensen Bernard <jensenbernard5@gmail.com>
2016-12-17 15:25:11 +0100

    Fix tests for Madux

--- test/Madux.test.js
+++ test/Madux.test.js
@@ -1,17 +1,18 @@
 
 // @flow
 
-import { createMachine, createState } from '../src/Madux';
+import { createMachine } from '../src/Madux';
 
 describe('Binder', () => {
-  const LOCKED = createState('LOCKED');
-  const UNLOCKED = createState('UNLOCKED');
+  const LOCKED = { name: 'LOCKED' };
+  const UNLOCKED = { name: 'UNLOCKED' };
   const LOCK = 'LOCK';
   const UNLOCK = 'UNLOCK';
 
   it('should work with basic use case', () => {
-    const machine = createMachine([UNLOCKED, LOCKED]);
+    const machine = createMachine(UNLOCKED, LOCKED);
     machine.from(LOCKED).to(UNLOCKED).on(UNLOCK);
     machine.from(UNLOCKED).to(LOCKED).on(LOCK);
+    machine.buildStore();
   });
 });
